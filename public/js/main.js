a5dcfd5aa4223e6b7f9a258e1ea5965fd5f54775
benva <benjamin.asals@gmail.com>
2016-12-28 16:49:46 -0500

    Styled modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
     var winHasChanged = function() {
-        var winHeight = $(window).height() - 75;
+        var winHeight = $(window).height() - 56;
         $(".content").css("margin-top", winHeight);
     };
+
+    $('.pop-up').on('click', function(e) {
+        $('.img-preview').attr('src', $(this).find('img').attr('src'));
+        $('#img-modal').modal('show');
+        e.preventDefault();
+    });
 
 
     // get rid of .load

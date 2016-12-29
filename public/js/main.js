cd3f8ea8a210c2edb84c3d1989e08458255d94d2
benva <benjamin.asals@gmail.com>
2016-12-28 19:00:33 -0500

    added photos and contact scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,33 @@
 $(document).ready(function() {
+    var winHeight = $(window).height();
+    var tabSize = 66;
     var winHasChanged = function() {
-        var winHeight = $(window).height() - 66;
-        $(".content").css("margin-top", winHeight);
+        $(".content").css("margin-top", winHeight - tabSize);
     };
+    // get rid of .load
+    // Change top margin on page load and resize
+    $(window).load("load", winHasChanged);
+    $(window).on("resize", winHasChanged);
 
+    // Show modal on image click
     $('.pop-up').on('click', function(e) {
         $('.img-preview').attr('src', $(this).find('img').attr('src'));
         $('#img-modal').modal('show');
         e.preventDefault();
     });
 
+    // Scroll animation
+    $("#photos").click(function(e) {
+        $("html, body").animate({
+            scrollTop: winHeight
+        }, 600, 'swing');
+        e.preventDefault();
+    });
 
-    // get rid of .load
-    $(window).load("load", winHasChanged);
-    $(window).on("resize", winHasChanged);
+    $("#contact").click(function(e) {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 750, 'swing');
+        e.preventDefault();
+    });
 });
